47078cbf2c51909e0d9a4c9d7dec2d03a254c7d7
Uma <umamulticloud24@gmail.com>
2026-09-21 15:48:48 +0530

    added storage account

---
+++ storage.tf
@@ -0,0 +1,18 @@
+resource "azurerm_storage_account" "example" {
+  name                = "storageaccountname"
+  resource_group_name = azurerm_resource_group.example.name
+
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.example.id]
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
